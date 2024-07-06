8c9531b93a3ed186af315c29a0c9b2f1f96bd069
Talal <talaldev@protonmail.com>
2024-07-06 10:33:52 +0200

    Rename

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TEI Web Services</title>
+  </head>
+  <body>
+    <!-- Nav (should collapse to sidebar in mobile view) -->
+    <div class="main">
+        <form action="/contact.php" method="POST">
+            <!-- form data -->
+        </form>
+    </div>
+    <!-- Footer -->
+  </body>
+</html>
