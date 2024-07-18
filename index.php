a5119a71f3d155ff770edd8f2a4b796780503900
Talal <talaldev@protonmail.com>
2024-07-18 15:42:27 +0200

    Added index and about pages.

--- index.php
+++ index.php
@@ -8,33 +8,45 @@ Done By Elteyp
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./assets/css/style.css" />
     <title>TEI Web Services</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <style>
+        .home-main {
+            width: 80%;
+        }
+
+        .intro {
+            padding: 5%;
+            width: 60%;
+            font-family: 'roboto';
+            font-size: 36px;
+            font-weight: bold;
+            /* color: white; */
+            /* background-color: rgba(75, 75, 100, 0.5); */
+            /* backdrop-filter: blur(20px); */
+        }
+
+        .btn-services {
+            padding: 1%;
+            font-size: 24px;
+            text-decoration: none;
+            color: black;
+            border: 1px solid black;
+        }
+
+    </style>
   </head>
   <body>
     <?php include('includes/header.inc.php'); ?>
     <main class="home-main">
-      <section class="home-section">
-        Two calls to action: Order | View Pricings
-      </section>
-      <section class="home-section">
-        <h2>We Design</h2>
-        <img src="./assets/images/ui-1.jpg" />
-      </section>
-      <section class="home-section">
-        <h2>We build web apps</h2>
-        <img src="./assets/images/fe-1.jpg" />
-      </section>
-      <section class="home-section">
-        <h2>We build mobile apps</h2>
-        <img src="./assets/images/ma-1.jpg" />
-      </section>
-      <section class="home-section">
-        <h2>We build back-end services</h2>
-        <img src="./assets/images/be-1.jpg" />
-      </section>
-      <section class="home-section">
-        <h2>We build full-stack applications from back to front</h2>
-        <img src="./assets/images/fs-1.jpg" />
-      </section>
+        <div class="intro">
+            We are a team of software developers who like to build and design
+            web and mobile applications. We developed web applications for popular
+            brands like X, Y, and Z.
+            <br><br>
+            <a class="btn-services" href="/services.php">Services</a>
+        </div>
     </main>
     <?php include('includes/footer.inc.php'); ?>
     <script src="./assets/js/script.js"></script>
